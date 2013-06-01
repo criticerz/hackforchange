86f1b4a9bb79f6cc9b73fbdc6c3d0e11fba3d83f
Alain Fontaine <criticerz@gmail.com>
2013-06-01 15:20:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackforchange::Application.config.secret_token = '7527fd418bfa1efd4d818b821452adbe16887d8a456473b9f58f5214b73f763415a0e0aa53502f0eddec00e1fd71711d1b7a1849f77f70d8ed6ce07a72fcc88a'
